6acbd3d751749cad15b418118a3032f82cab7d01
martin-ngigi <martinwainaina001@gmail.com>
2023-01-18 21:39:08 +0300

    Initialized Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+import 'package:chat_app_flutter/shared/constants.dart';
 
-void main(){
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+
+  //condition to check the where the plaltform the app is running in i.e. android, web, iOS
+  if(kIsWeb){
+    //run initialization for web
+    await Firebase.initializeApp(options: FirebaseOptions(
+        apiKey: Constants.apiKey,
+        appId: Constants.appId,
+        messagingSenderId: Constants.messagingSenderId,
+        projectId: Constants.projectId
+    ));
+  }
+  else{
+    //run initialization for iOS and android
+    await Firebase.initializeApp();
+  }
+
   runApp(MaterialApp(
     home: MyApplication(),
   ));
---
+++ lib/shared/constants.dart
@@ -0,0 +1,14 @@
+class Constants{
+  static String apiKey="AIzaSyAsVF6zYpzrJ7qsgxm8v9uFVmEH0ucaBvU";
+  static String appId="1:137098852608:web:844703ed47b2ce82bd1be5";
+  static String messagingSenderId="137098852608";
+  static String projectId="chat-app-flutter-b103e";
+
+
+
+
+  static String authDomain= "chat-app-flutter-b103e.firebaseapp.com";
+  static String storageBucket= "chat-app-flutter-b103e.appspot.com";
+  static String measurementId= "G-GXGVVBYTJH";
+
+}
