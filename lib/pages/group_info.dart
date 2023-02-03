531e7057df84766ab371a4037826a9f1011b3440
martin-ngigi <martinwainaina001@gmail.com>
2023-02-03 19:07:03 +0300

    Exit roup Chat success

--- lib/pages/group_info.dart
+++ lib/pages/group_info.dart
@@ -2,6 +2,9 @@ import 'package:chat_app_flutter/pages/services/database_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/widgets.dart';
+import 'home_page.dart';
+
 //stful
 class GroupInfo extends StatefulWidget {
   final String groupId;
@@ -58,9 +61,48 @@ class _GroupInfoState extends State<GroupInfo> {
         title: Text("Group info"),
         actions: [
           IconButton(
-              onPressed: (){},
-              icon: Icon(Icons.exit_to_app),
-          ),
+              onPressed: () {
+                showDialog(
+                    barrierDismissible: false,
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: const Text("Exit"),
+                        content:
+                        const Text("Are you sure you exit the group? "),
+                        actions: [
+                          IconButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            icon: const Icon(
+                              Icons.cancel,
+                              color: Colors.red,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () async {
+                              DatabaseService(
+                                  uid: FirebaseAuth
+                                      .instance.currentUser!.uid)
+                                  .toggleGroupJoin(
+                                  widget.groupId,
+                                  getName(widget.adminName),
+                                  widget.groupName)
+                                  .whenComplete(() {
+                                nextScreenReplace(context, const HomePage());
+                              });
+                            },
+                            icon: const Icon(
+                              Icons.done,
+                              color: Colors.green,
+                            ),
+                          ),
+                        ],
+                      );
+                    });
+              },
+              icon: const Icon(Icons.exit_to_app))
         ],
       ),
       body: Container(
